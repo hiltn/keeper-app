8fe959b137f23bda217888df27cd21d6727aff5e
hiltn <mounaelherdd@gmail.com>
2023-11-03 20:17:44 +0100

    Initial commit

---
+++ src/compenents/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function note() {
+    return (
+        <div className="note">
+            <h2>this is the note tite</h2>
+            <p>note</p>
+        </div>
+    );
+}
+
+export default note;
